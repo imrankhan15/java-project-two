1a92281a5f5bf54d6c2dc6bae7619b56f80118b6
Muhammad Faisal Imran Khan <buetcse110@gmail.com>
2022-03-16 15:52:46 +0100

    first commit

---
+++ src/Animal.java
@@ -0,0 +1,15 @@
+public class Animal {
+
+    public static String iAmHuman(){
+        return "Human";
+    }
+
+    public static void doStuff(){
+        int a = 5;
+
+        while (a < 50){
+            System.out.println("hi");
+            a++;
+        }
+    }
+}
---
+++ src/test.java
@@ -0,0 +1,12 @@
+public class test {
+
+    public static void main(String[] args){
+      Animal a = new Animal();
+     a.doStuff();
+
+    }
+
+    public static void addExclamationPoint(String s){
+        System.out.println(s + "!");
+    }
+}
